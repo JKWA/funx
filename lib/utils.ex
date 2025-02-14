c1445d07703d8bae5522a2c94f9335b0fb662641
Joseph Koski <joe.koski@gmail.com>
2025-02-14 13:11:52 -0800

    Add curry_r

--- lib/utils.ex
+++ lib/utils.ex
@@ -6,6 +6,7 @@ defmodule Monex.Utils do
   programming style. It includes:
 
   - `curry/1`: Transforms a multi-argument function into a curried version.
+  - `curry_r/1`: Transforms a multi-argument function into a curried version, but from right to left.
   - `flip/1`: Reverses the argument order of a two-argument function.
   """
 
@@ -32,6 +33,46 @@ defmodule Monex.Utils do
 
   defp curry(fun, arity, args) when arity > 1 do
     fn next_arg -> curry(fun, arity - 1, args ++ [next_arg]) end
+  end
+
+  @doc """
+  Alias for `curry/1`, explicitly denoting left-to-right argument application.
+
+  ## Example
+
+      iex> subtract = fn a, b -> a - b end
+      iex> curried_subtract = FunPark.Utils.curry_l(subtract)
+      iex> subtract_five = curried_subtract.(5)
+      iex> subtract_five.(3)
+      2
+  """
+  @spec curry_l((... -> any())) :: any()
+
+  def curry_l(fun) when is_function(fun), do: curry(fun)
+
+  @doc """
+  Transforms a function of arity `n` into a right-curried version,
+  applying arguments from right to left.
+
+  ## Example
+
+      iex> divide = fn a, b -> a / b end
+      iex> curried_divide = FunPark.Utils.curry_r(divide)
+      iex> divide_by_two = curried_divide.(2)
+      iex> divide_by_two.(10)
+      5.0
+  """
+  @spec curry_r((... -> any())) :: any()
+  def curry_r(fun) when is_function(fun) do
+    arity = :erlang.fun_info(fun, :arity) |> elem(1)
+    curry_r(fun, arity, [])
+  end
+
+  defp curry_r(fun, 1, args),
+    do: fn last_arg -> apply(fun, [last_arg | args]) end
+
+  defp curry_r(fun, arity, args) when arity > 1 do
+    fn next_arg -> curry_r(fun, arity - 1, [next_arg | args]) end
   end
 
   @doc """
--- test/utils_test.exs
+++ test/utils_test.exs
@@ -34,6 +34,58 @@ defmodule Monex.UtilsTest do
     end
   end
 
+  describe "curry_l/1" do
+    test "curry_l is identical to curry" do
+      add = fn a, b -> a + b end
+      curried_add_l = curry_l(add)
+      curried_add = curry(add)
+
+      assert curried_add_l.(5).(3) == curried_add.(5).(3)
+    end
+
+    test "curry_l applies arguments left-to-right" do
+      subtract = fn a, b -> a - b end
+      curried_subtract_l = curry_l(subtract)
+
+      assert is_function(curried_subtract_l)
+      subtract_five = curried_subtract_l.(5)
+      assert is_function(subtract_five)
+      assert subtract_five.(3) == 2
+    end
+  end
+
+  describe "curry_r/1" do
+    test "right curries a function with arity 2" do
+      subtract = fn a, b -> a - b end
+      curried_subtract = curry_r(subtract)
+
+      assert is_function(curried_subtract)
+      minus_three = curried_subtract.(3)
+      assert is_function(minus_three)
+      assert minus_three.(10) == 7
+    end
+
+    test "right curries a function with arity 3" do
+      divide = fn a, b, c -> a / b / c end
+      curried_divide = curry_r(divide)
+
+      assert is_function(curried_divide)
+      step1 = curried_divide.(2)
+      assert is_function(step1)
+      step2 = step1.(4)
+      assert is_function(step2)
+      assert step2.(16) == 2.0
+    end
+
+    test "right currying works with a single argument function" do
+      identity = fn x -> x end
+      curried_identity = curry_r(identity)
+
+      assert is_function(curried_identity)
+      assert curried_identity.(42) == 42
+    end
+  end
+
   describe "flip/1" do
     test "flips a function with arity 2" do
       subtract = fn a, b -> a - b end
